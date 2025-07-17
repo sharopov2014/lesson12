0dccfe24d44f9ee36a8af3e0049d49830b8e4791
sharopov2014 <sharopovalizod@gmail.com>
2025-07-17 09:01:41 +0500

    first commit

--- js/main.js
+++ js/main.js
@@ -7,21 +7,21 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-let modeBtn = document.getElementById("mode-btn");
+// let modeBtn = document.getElementById("mode-btn");
 
-modeBtn.addEventListener("click", function () {
-  if (document.body.className != "dark") {
-    this.firstElementChild.src = "./images/sun.svg";
-  } else {
-    this.firstElementChild.src = "./images/moon.svg";
-  }
-  document.body.classList.toggle("dark");
-});
+// modeBtn.addEventListener("click", function () {
+//   if (document.body.className != "dark") {
+//     this.firstElementChild.src = "./images/sun.svg";
+//   } else {
+//     this.firstElementChild.src = "./images/moon.svg";
+//   }
+//   document.body.classList.toggle("dark");
+// });
 
-const loading = document.getElementById("loading");
+// const loading = document.getElementById("loading");
 
-const loadingDuration = 2000; // 2s
+// const loadingDuration = 2000; // 2s
 
-setTimeout(() => {
-  loading.classList.add("loading-none");
-}, loadingDuration);
+// setTimeout(() => {
+//   loading.classList.add("loading-none");
+// }, loadingDuration);
